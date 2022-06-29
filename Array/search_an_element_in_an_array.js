554cc5f04e990e8af351775f3020dd2df2cffe1b
Vijay Benz <benzwrites@gmail.com>
2022-06-29 19:34:31 +0530

    Create search_an_element_in_an_array.js

---
+++ Array/search_an_element_in_an_array.js
@@ -0,0 +1,10 @@
+class Solution {
+    search(arr, N, X) {
+        for(let i=0;i<N;i++){
+            if(arr[i] === X){
+                return i
+            }
+        }
+        return -1
+    }
+}
